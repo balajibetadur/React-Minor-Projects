0ae764d4d9826f5174e5ad589787f6be09888d8c
Balaji Betadur <=>
2022-06-28 12:11:57 +0530

    Flip Game Implemented

--- src/Amazon/Subtotal/Subtotal.js
+++ src/Amazon/Subtotal/Subtotal.js
@@ -1,15 +1,15 @@
 import React from "react";
-import './Subtotal.css'
+import "./Subtotal.css";
 
 function Subtotal() {
   return (
     <div className="subtotal">
-      <div className="subtotal__amount">
-        Subtotal (0 items): $<strong>0</strong>
+      <div className="subtotal__header">
+        <div className="subtotal__amount">
+          Subtotal (0 items): $<strong>0</strong>
+        </div>
+        <button className="subtotal__button">Proceed to Checkout</button>
       </div>
-      <button>
-        Proceed to Checkout
-      </button>
     </div>
   );
 }
